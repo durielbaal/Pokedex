00396c6195351fabb6c46ea88a039c12224b2a71
durielbaal <michaelogwu1997@gmail.com>
2024-12-09 22:36:41 +0100

    6.- Get pokemon from database.

--- src/main/java/com/myke/studios/application/controller/PokemonController.java
+++ src/main/java/com/myke/studios/application/controller/PokemonController.java
@@ -1,8 +1,13 @@
 package com.myke.studios.application.controller;
 
+import com.myke.studios.domain.entity.Pokemon;
 import com.myke.studios.domain.service.PokemonService;
 import com.myke.studios.infraestructure.output.PokemonOutputPort;
 import com.myke.studios.shared.Constants;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +16,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 @Data
 @RequiredArgsConstructor
-@RestController
+@RestController(Constants.POKEMON_OWN_BASE)
 public class PokemonController  {
 
   private final PokemonOutputPort pokemonOutputPort;
 
-  @GetMapping(path = Constants.HOENN_REGION)
+  @GetMapping(path = Constants.HOENN_SAVE_REGION)
   public ResponseEntity<String> getPokemonFromTo() {
     ResponseEntity<String> responseEntity = ResponseEntity.ok("Pokemon of Hoenn were stored correctly");
     try{
@@ -26,8 +31,24 @@ public class PokemonController  {
     }
     return responseEntity;
   }
+  @GetMapping(path = Constants.GET_HOENN_POKEDEX)
+  public ResponseEntity<Map<String,List<Pokemon>>> getHoennPokedex(){
+    Map<String, List<Pokemon>> map = new HashMap<>();
+    try{
+      List<Pokemon> hoennPokedex = pokemonOutputPort.getAllPokemon();
+      map.put("Pokedex of Hoenn completed", hoennPokedex);
+    } catch (Exception e) {
+      List<Pokemon> hoennPokedex = new ArrayList<>();
+      map.put("Something was wrong...", hoennPokedex);
+      return ResponseEntity.badRequest().body(map);
+    }
+    return ResponseEntity.ok(map);
+  }
+
   @GetMapping(Constants.TEST)
   public ResponseEntity<String> testing() {
-    return  ResponseEntity.ok("This is a test, and works :)");
+    return  ResponseEntity.ok("This is a test, and it works :)");
   }
+
+
 }
--- src/main/java/com/myke/studios/shared/Constants.java
+++ src/main/java/com/myke/studios/shared/Constants.java
@@ -4,6 +4,8 @@ public class Constants {
   public static final String POKEMON_BASE_URL = "https://pokeapi.co/api/v2/";
   public static final String POKEMON_PARAMETERS = "pokemon/{id}";
   public static final String POKEMON_DETAILS = "pokemon-species/{id}";
-  public static final String HOENN_REGION = "pokedex/hoenn";
+  public static final String POKEMON_OWN_BASE = "/pokedex/";
+  public static final String HOENN_SAVE_REGION = "store/Hoenn";
+  public static final String GET_HOENN_POKEDEX = "get/Hoenn";
   public static final String TEST = "pokedex/test";
 }
